68ff89bea4243d9d738223128f53a58054d3cf28
harrietcraven <harrietcraven@Harriets-MacBook-Pro.local>
2015-08-31 16:37:22 +0100

    Blog update at 2015-08-31 15:37:22

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,55 +2,18 @@
  * Main JS file for Casper behaviours
  */
 
-/* globals jQuery, document */
-(function ($, undefined) {
+/*globals jQuery, document */
+(function ($) {
     "use strict";
 
-    var $document = $(document);
+    $(document).ready(function(){
 
-    $document.ready(function () {
+        // On the home page, move the blog icon inside the header 
+        // for better relative/absolute positioning.
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
-
-        $(".scroll-down").arctic_scroll();
-
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
-        });
+        //$("#blog-logo").prependTo("#site-head-content");
+        hljs.initHighlightingOnLoad();
 
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
-
-    };
-})(jQuery);
+}(jQuery));
